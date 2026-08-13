174b76d02638836a53571a25e89fb1631596a9cb
Putra <surya.putra11@gmail.com>
2026-08-13 19:49:28 +0700

    update baru

--- function.js
+++ function.js
@@ -1,27 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const elementP = document.getElementById("typing-text");
-    const source = document.getElementById("source-text");
+fetch('header.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
 
-    const htmlText = source.innerHTML.trim();      // versi lengkap dengan <b>
-    const plainText = source.textContent.trim();   // versi tanpa tag, buat animasi ngetik
+document.addEventListener("DOMContentLoaded", function () {
+    const elementP1 = document.getElementById("typing-text-1");
+    const elementP2 = document.getElementById("typing-text-2");
+
+    const sourceP1 = document.getElementById("source-text-1");
+    const sourceP2 = document.getElementById("source-text-2");
+
+    const htmlTextP1 = sourceP1.innerHTML.trim();
+    const plainTextP1 = sourceP1.textContent.trim();
+
+    const htmlTextP2 = sourceP2.innerHTML.trim();
+    const plainTextP2 = sourceP2.textContent.trim();
 
     let index = 0;
     const kecepatan = 35;
-    const jedaUlang = 3000;
 
-    function ketik() {
-        if (index <= plainText.length) {
-            elementP.textContent = plainText.substring(0, index);
+    function ketikP1() {
+        if (index <= plainTextP1.length) {
+            elementP1.textContent = plainTextP1.substring(0, index);
             index++;
-            setTimeout(ketik, kecepatan);
+            setTimeout(ketikP1, kecepatan);
         } else {
-            elementP.innerHTML = htmlText; //kata2 akan otomatis berubah menjadi versi lengkap dengan <b> setelah selesai ngetik
-            setTimeout(() => {
-                index = 0;
-                ketik();
-            }, jedaUlang);
+            elementP1.innerHTML = htmlTextP1;
+
+            index = 0;
+            ketikP2();
         }
     }
 
-    ketik();
+    function ketikP2() {
+        if (index <= plainTextP2.length) {
+            elementP2.textContent = plainTextP2.substring(0, index);
+            index++;
+            setTimeout(ketikP2, kecepatan);
+        } else {
+            elementP2.innerHTML = htmlTextP2;
+        }
+    }
+
+    ketikP1();
 });
